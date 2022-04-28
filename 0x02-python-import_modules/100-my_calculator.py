b624dc7fce88dd007e67b3f4107669c9d3b59625
KevinFRX <xerokf13@gmail.com>
2022-04-28 16:15:20 -0700

    2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     elif sys.argv[2] == "+":
         print(int(sys.argv[1]), "+", int(sys.argv[3]), "=", add(int(sys.argv[1]), int(sys.argv[3])))
     elif sys.argv[2] == "-":
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         print(int(sys.argv[1]), "/", int(sys.argv[3]), "=", div(int(sys.argv[1]), int(sys.argv[3])))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
