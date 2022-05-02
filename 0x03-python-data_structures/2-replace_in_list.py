e29946ebfaad112aafc60ccca6d8021df48857ae
KevinFRX <xerokf13@gmail.com>
2022-05-02 10:13:46 -0700

    1

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in my_list:
+        pass
+    if idx < 0 or idx >= i:
+        return(my_list)
+    my_list[idx] = element
+    return(my_list)
