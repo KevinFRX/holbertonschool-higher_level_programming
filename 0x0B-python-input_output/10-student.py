76f2c1108cdb4e4963a28507b67f4fcc262a62f2
KevinFRX <xerokf13@gmail.com>
2022-05-31 17:33:16 -0700

    1

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is str:
+                    dictry = {}
+                    for key, value in self.__dict__.items():
+                        if key in attrs:
+                            dictry[key] = value
+                    return dictry
+        return self.__dict__
