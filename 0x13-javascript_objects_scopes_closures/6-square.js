9e7c0b2dcb10fb0ae99ba37a88e2307f6721413d
KevinFRX <xerokf13@gmail.com>
2022-08-02 13:34:03 -0700

    4

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
-class Square extends require('./5-square.js') {
+class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
     if (c === undefined) {
       this.print();
