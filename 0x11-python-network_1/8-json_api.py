8ccd22dccf21ef2d659445556beff0227eda3779
KevinFRX <xerokf13@gmail.com>
2022-08-28 06:30:10 -0700

    1

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    resp = requests.post(url, q)
+
+    try:
+        jason = resp.json()
+        if jason == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(jason.get("id"), jason.get("name")))
+    except:
+        print("Not a valid JSON")
